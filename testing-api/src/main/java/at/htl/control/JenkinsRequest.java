5e74966e2178f5583950aeb778156a5dc2471dab
flo-kei <florian.keintzel@gmx.at>
2022-06-18 12:10:30 +0200

    testing-api is now waiting for jenkins build to finish

--- testing-api/src/main/java/at/htl/control/JenkinsRequest.java
+++ testing-api/src/main/java/at/htl/control/JenkinsRequest.java
@@ -3,6 +3,8 @@ package at.htl.control;
 import com.cdancy.jenkins.rest.JenkinsClient;
 import com.cdancy.jenkins.rest.domain.system.SystemInfo;
 
+import java.io.IOException;
+
 /*
 https://www.jenkins.io/doc/book/using/remote-access-api/
 https://github.com/cdancy/jenkins-rest
@@ -14,20 +16,60 @@ public class JenkinsRequest {
     private static final String ADMIN_TOKEN = "11d1a936ffef3dbc7856d7d963dade76d2";
     private static final String MVN_PROJECT_TOKEN = "avFPxhFrSm6a5UQHm3Kf2Ge4oYzdz3ESuF7igg2v";
 
+    private static final String JENKINS_JOB = "mvn-project";
+
     private static final String JENKINS_URL = "http://jenkins:8080";
 
-    private static final String URL = JENKINS_URL+"/job/mvn-project/build?token="+MVN_PROJECT_TOKEN;
+    // for local testing only:
+    //private static final String JENKINS_URL = "http://localhost:8081";
+
+    private static final String BUILD_URL = JENKINS_URL+"/job/"+JENKINS_JOB+"/build?token="+MVN_PROJECT_TOKEN;
+
+    private static int currentJobNumber = -1;
 
     public static void main(String[] args) {
         sendRequest();
     }
 
     public static void sendRequest() {
+        // create jenkins rest-client instance
         JenkinsClient client = JenkinsClient.builder()
-                .endPoint(URL)
+                .endPoint(JENKINS_URL)
                 .credentials("admin:"+ADMIN_TOKEN)
                 .build();
 
-        SystemInfo systemInfo = client.api().systemApi().systemInfo();
+        // store current queue size
+        var prevSize = client.api().queueApi().queue().size();
+
+        // tell jenkins to start the build
+        var response = client.api().jobsApi().build(null, JENKINS_JOB);
+        int currSize = Integer.MAX_VALUE;
+
+        // wait for build to appear in queue
+        while(currSize != prevSize){
+            currSize = client.api().queueApi().queue().size();
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        // wait for build to finish
+        currentJobNumber = client.api().jobsApi().lastBuildNumber(null,JENKINS_JOB);
+        var buildInfo = client.api().jobsApi().buildInfo(null, JENKINS_JOB,currentJobNumber);
+        while(buildInfo.building()){
+            buildInfo = client.api().jobsApi().buildInfo(null, JENKINS_JOB,currentJobNumber);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        // log buildInfo for debugging purposes
+        /*buildInfo = client.api().jobsApi().buildInfo(null, JENKINS_JOB,currentJobNumber);
+        System.out.println(buildInfo);*/
     }
+
 }
